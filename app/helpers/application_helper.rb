dc8a97a3ddb244a2f6ed4299d4cb9b8ba482b755
clarkey022 <roryfrancisclark@gmail.com>
2023-06-21 18:27:02 +0100

    Added view page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+    def convert_loan_status(loan_status)
+        if loan_status === true
+          "On loan"
+        else
+          "Available"
+        end
+      end
+
+    def remove_trailing_UTC(created_at)
+        created_at.strftime("%a, %d %b %Y %H:%M:%S")
+    end
+
+    def encrich_onLoan_text(user_id)
+        if user_id != nil 
+            "This book is on loan to user " + user_id
+        else 
+            "This book is not on loan to anyone"
+        end
+    end
 end
